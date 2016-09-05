665cf862e075a9dd9489eb5c8803a2560099e27e
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2016-09-05 10:17:08 -0400

    Adding in the Hooray and Weeee programs to the repository.

---
+++ src/mine/Hooray.java
@@ -0,0 +1,13 @@
+public class Hooray {
+
+    public static void hooray() {
+        while(true) {
+            System.out.println("Hooray!");
+        }
+    }
+
+    public static void main(String[] args) {
+        hooray();
+    }
+
+}
---
+++ src/mine/Weeee.java
@@ -0,0 +1,12 @@
+public class Weeee {
+
+    public static void weeee() {
+        System.out.println("Weeee!");
+        weeee();
+    }
+
+    public static void main(String[] args) {
+        weeee();
+    }
+
+}

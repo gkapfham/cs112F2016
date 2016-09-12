48cbdd01bc636dbad4ea6980888aade4ef2e1172
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2016-09-11 20:23:52 -0400

    Updating the indenting in the SentencesReverser.

--- src/mine/labs/lab2/SentencesReverser.java
+++ src/mine/labs/lab2/SentencesReverser.java
@@ -4,14 +4,14 @@ import java.util.Iterator;
 
 public class SentencesReverser {
 
-    public static void main(String[] args) throws IOException {
-        Scanner scanner = new Scanner(System.in);
-        Sentences sentences = new Sentences();
+  public static void main(String[] args) throws IOException {
+    Scanner scanner = new Scanner(System.in);
+    Sentences sentences = new Sentences();
 
-        sentences.readSentencesFromFile();
-        sentences.printSentences();
-        sentences.reverseSentences();
-        sentences.printSentences();
-    }
+    sentences.readSentencesFromFile();
+    sentences.printSentences();
+    sentences.reverseSentences();
+    sentences.printSentences();
+  }
 
 }

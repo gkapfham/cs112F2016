72f649f10d5ef3e8d0dc10591dc12deda677f055
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2016-09-12 09:45:19 -0400

    Adding in some instrumentation as an example to the SentencesReverser.

--- src/mine/labs/lab2/SentencesReverser.java
+++ src/mine/labs/lab2/SentencesReverser.java
@@ -8,7 +8,11 @@ public class SentencesReverser {
     Scanner scanner = new Scanner(System.in);
     Sentences sentences = new Sentences();
 
+    long beforeRead = System.currentTimeMillis();
     sentences.readSentencesFromFile();
+    long afterRead = System.currentTimeMillis();
+    System.out.println("Read time: " + (afterRead - beforeRead));
+
     sentences.printSentences();
     sentences.reverseSentences();
     sentences.printSentences();

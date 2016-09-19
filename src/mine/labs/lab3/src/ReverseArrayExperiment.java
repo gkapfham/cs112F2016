a143420a073917ad30e39bff5999982d52aecc9e
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2016-09-18 22:58:43 -0400

    Adding in all of the needed timing code and then starting the nested class in the ReverseArrayExperiment.

--- src/mine/labs/lab3/src/ReverseArrayExperiment.java
+++ src/mine/labs/lab3/src/ReverseArrayExperiment.java
@@ -3,6 +3,10 @@ import java.util.Collections;
 
 public class ReverseArrayExperiment {
 
+private static class StatisticsCalculator {
+
+}
+
   public static <T> void reverse(T[] data) {
     int low = 0, high = data.length - 1;
     while (low < high) {                   // swap data[low] and data[high]
@@ -21,60 +25,84 @@ public class ReverseArrayExperiment {
 
     Long[] integerTimings = new Long[trials];
     Long[] floatTimings = new Long[trials];
-    Long[] integerTimingsProvided = new Long[trials];
+    Long[] integerTimingsCollections = new Long[trials];
+    Long[] floatTimingsCollections = new Long[trials];
 
     System.out.println("Timing Random Integer[]\n");
     Integer[] randomIntegerArray = new Integer[size];
     randomIntegerArray = RandomArrayGenerator.generateRandomIntegerArray(size);
     ArrayPrinter.print(randomIntegerArray, indentation, debug);
 
+    System.out.println("Provided Trials: ");
+    System.out.print(indentation);
     for(int i = 0; i < trials; i++) {
       long beforeIntegerReverse = System.nanoTime();
       ReverseArray.reverse(randomIntegerArray);
       long afterIntegerReverse = System.nanoTime();
       integerTimings[i] = new Long(afterIntegerReverse - beforeIntegerReverse);
+      System.out.print(".");
     }
 
+    System.out.println("\nCollections Trials: ");
+    System.out.print(indentation);
     for(int i = 0; i < trials; i++) {
       long beforeIntegerReverse = System.nanoTime();
       Collections.reverse(Arrays.asList(randomIntegerArray));
       long afterIntegerReverse = System.nanoTime();
-      integerTimingsProvided[i] = new Long(afterIntegerReverse - beforeIntegerReverse);
+      integerTimingsCollections[i] = new Long(afterIntegerReverse - beforeIntegerReverse);
+      System.out.print(".");
     }
 
     System.out.println();
     ArrayPrinter.print(randomIntegerArray, indentation, debug);
     System.out.println();
 
-    System.out.println("\nTiming Random Float[]\n");
+    System.out.println("Timing Random Float[]\n");
     Float[] randomFloatArray = new Float[size];
     randomFloatArray = RandomArrayGenerator.generateRandomFloatArray(size);
     ArrayPrinter.print(randomFloatArray, indentation, debug);
 
+    System.out.println("Provided Trials: ");
+    System.out.print(indentation);
     for(int i = 0; i < trials; i++) {
       long beforeFloatReverse = System.nanoTime();
       ReverseArray.reverse(randomFloatArray);
       long afterFloatReverse = System.nanoTime();
       floatTimings[i] = afterFloatReverse - beforeFloatReverse;
+      System.out.print(".");
+    }
+
+    System.out.println("\nCollections Trials: ");
+    System.out.print(indentation);
+    for(int i = 0; i < trials; i++) {
+      long beforeFloatReverse = System.nanoTime();
+      Collections.reverse(Arrays.asList(randomFloatArray));
+      long afterFloatReverse = System.nanoTime();
+      floatTimingsCollections[i] = new Long(afterFloatReverse - beforeFloatReverse);
+      System.out.print(".");
     }
 
     System.out.println();
     ArrayPrinter.print(randomFloatArray, indentation, debug);
-    System.out.println("\n");
-
-    System.out.println("Timing Results:");
     System.out.println();
 
-    System.out.println("Integer reversal (ns):");
+    System.out.println("Summary of Timing Results:");
+    System.out.println();
+
+    System.out.println("Integer Reversal Provided (ns):");
     ArrayPrinter.print(integerTimings, indentation, always);
     System.out.println();
 
-    System.out.println("Integer reversal provided (ns):");
-    ArrayPrinter.print(integerTimingsProvided, indentation, always);
+    System.out.println("Integer Reversal Collections (ns):");
+    ArrayPrinter.print(integerTimingsCollections, indentation, always);
     System.out.println();
 
-    System.out.println("Float reversal (ns): ");
+    System.out.println("Float Reversal Provided (ns): ");
     ArrayPrinter.print(floatTimings, indentation, always);
+    System.out.println();
+
+    System.out.println("Float Reversal Collections (ns): ");
+    ArrayPrinter.print(floatTimingsCollections, indentation, always);
 
   }
 

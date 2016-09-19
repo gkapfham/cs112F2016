1385caaa947ec526e533836da4b39ca1babb399f
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2016-09-18 21:31:06 -0400

    Changing the name of the file so that there is now a ReverseArrayExperiment.

---
+++ src/mine/labs/lab3/src/ReverseArrayExperiment.java
@@ -0,0 +1,34 @@
+public class ReverseArrayExperiment {
+
+  public static <T> void reverse(T[] data) {
+    int low = 0, high = data.length - 1;
+    while (low < high) {                   // swap data[low] and data[high]
+      T temp = data[low];
+      data[low++] = data[high];            // post-increment of low
+      data[high--] = temp;                 // post-decrement of high
+    }
+  }
+
+  public static void main(String[] args) {
+    String indentation = new String("  ");
+    int size = 100;
+
+    System.out.println("Random Integer[]\n");
+    Integer[] randomIntegerArray = new Integer[size];
+    randomIntegerArray = RandomArrayGenerator.generateRandomIntegerArray(10);
+    RandomArrayGenerator.ArrayPrinter.print(randomIntegerArray, indentation);
+    ReverseArray.reverse(randomIntegerArray);
+    System.out.println();
+    RandomArrayGenerator.ArrayPrinter.print(randomIntegerArray, indentation);
+
+    System.out.println("\nRandom Float[]\n");
+    Float[] randomFloatArray = new Float[size];
+    randomFloatArray = RandomArrayGenerator.generateRandomFloatArray(10);
+    RandomArrayGenerator.ArrayPrinter.print(randomFloatArray, indentation);
+    ReverseArray.reverse(randomFloatArray);
+    System.out.println();
+    RandomArrayGenerator.ArrayPrinter.print(randomFloatArray, indentation);
+
+  }
+
+}

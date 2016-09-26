c263a9a9e07e372dd39d3c95c26e32346e504675
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2016-09-25 21:33:31 -0400

    Removing all of the extraneous code from the SortArrayExperiment, also adding in the correct calls to the new generator.

--- src/mine/labs/lab4/src/edu/allegheny/experiment/SortArrayExperiment.java
+++ src/mine/labs/lab4/src/edu/allegheny/experiment/SortArrayExperiment.java
@@ -32,83 +32,34 @@ public class SortArrayExperiment {
 
   }
 
-  public static <T> void reverse(T[] data) {
-    int low = 0, high = data.length - 1;
-    while (low < high) {                   // swap data[low] and data[high]
-      T temp = data[low];
-      data[low++] = data[high];            // post-increment of low
-      data[high--] = temp;                 // post-decrement of high
-    }
-  }
-
   public static void main(String[] args) {
     String indentation = new String("  ");
-    boolean debug = false;
+    boolean debug = true;
     boolean always = true;
-    int size = 1000;
-    int trials = 10;
+    int size = 10;
+    int trials = 1;
 
     Long[] integerTimings = new Long[trials];
     Long[] floatTimings = new Long[trials];
     Long[] integerTimingsCollections = new Long[trials];
     Long[] floatTimingsCollections = new Long[trials];
 
-    System.out.println("Timing Random Integer[]\n");
-    Integer[] randomIntegerArray = new Integer[size];
-    randomIntegerArray = RandomArrayGenerator.generateRandomIntegerArray(size);
-    ArrayPrinter.print(randomIntegerArray, indentation, debug);
+    Character[] randomCharacterArray = new Character[size];
+    randomCharacterArray = RandomArrayGenerator.generateRandomCharacterArray(size);
+    ArrayPrinter.print(randomCharacterArray, indentation, debug);
 
-    System.out.println("Provided Trials: ");
+    System.out.println("InsertionSort Trials: ");
     System.out.print(indentation);
     for (int i = 0; i < trials; i++) {
       long beforeIntegerReverse = System.nanoTime();
-      SortArrayExperiment.reverse(randomIntegerArray);
+      InsertionSort.insertionSort(randomCharacterArray);
       long afterIntegerReverse = System.nanoTime();
       integerTimings[i] = new Long(afterIntegerReverse - beforeIntegerReverse);
       System.out.print(".");
     }
 
-    System.out.println("\nCollections Trials: ");
-    System.out.print(indentation);
-    for(int i = 0; i < trials; i++) {
-      long beforeIntegerReverse = System.nanoTime();
-      Collections.reverse(Arrays.asList(randomIntegerArray));
-      long afterIntegerReverse = System.nanoTime();
-      integerTimingsCollections[i] = new Long(afterIntegerReverse - beforeIntegerReverse);
-      System.out.print(".");
-    }
-
     System.out.println();
-    ArrayPrinter.print(randomIntegerArray, indentation, debug);
-    System.out.println();
-
-    System.out.println("Timing Random Float[]\n");
-    Float[] randomFloatArray = new Float[size];
-    randomFloatArray = RandomArrayGenerator.generateRandomFloatArray(size);
-    ArrayPrinter.print(randomFloatArray, indentation, debug);
-
-    System.out.println("Provided Trials: ");
-    System.out.print(indentation);
-    for(int i = 0; i < trials; i++) {
-      long beforeFloatReverse = System.nanoTime();
-      SortArrayExperiment.reverse(randomFloatArray);
-      long afterFloatReverse = System.nanoTime();
-      floatTimings[i] = afterFloatReverse - beforeFloatReverse;
-      System.out.print(".");
-    }
-
-    System.out.println("\nCollections Trials: ");
-    System.out.print(indentation);
-    for (int i = 0; i < trials; i++) {
-      long beforeFloatReverse = System.nanoTime();
-      Collections.reverse(Arrays.asList(randomFloatArray));
-      long afterFloatReverse = System.nanoTime();
-      floatTimingsCollections[i] = new Long(afterFloatReverse - beforeFloatReverse);
-      System.out.print(".");
-    }
-
-    System.out.println();
-    ArrayPrinter.print(randomFloatArray, indentation, debug);
+    ArrayPrinter.print(randomCharacterArray, indentation, debug);
     System.out.println();
 
     System.out.println("Summary of Timing Results:");
@@ -120,20 +71,5 @@ public class SortArrayExperiment {
     System.out.println(indentation + "Standard Deviation (ns): " + StatisticsCalculator.calculateStandardDeviation(integerTimings));
     System.out.println();
 
-    System.out.println("Integer Reversal Collections (ns):");
-    ArrayPrinter.print(integerTimingsCollections, indentation, always);
-    System.out.println(indentation + "Arithmetic Mean (ns): " + StatisticsCalculator.calculateArithmeticMean(integerTimingsCollections));
-    System.out.println();
-
-    System.out.println("Float Reversal Provided (ns): ");
-    ArrayPrinter.print(floatTimings, indentation, always);
-    System.out.println(indentation + "Arithmetic Mean (ns): " + StatisticsCalculator.calculateArithmeticMean(floatTimings));
-    System.out.println();
-
-    System.out.println("Float Reversal Collections (ns): ");
-    ArrayPrinter.print(floatTimingsCollections, indentation, always);
-    System.out.println(indentation + "Arithmetic Mean (ns): " + StatisticsCalculator.calculateArithmeticMean(floatTimingsCollections));
-
   }
-
 }
